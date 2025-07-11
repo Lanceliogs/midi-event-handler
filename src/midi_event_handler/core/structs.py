ab6fcf70e677cd53c58e641807b169a1cb759cf6
Julien LANCELOT <julien.lancelot@enovasense.com>
2025-07-11 09:46:40 +0200

    minor: event related dataclasses added

--- src/midi_event_handler/core/structs.py
+++ src/midi_event_handler/core/structs.py
@@ -1,2 +1,21 @@
 from dataclasses import dataclass
+from typing import Optional, Dict, List
+
+@dataclass
+class MidiMessage:
+    type: str
+    note: int
+    velocity: int
+
+@dataclass
+class MidiEvent:
+    name: str
+    type: str
+    port: str
+    keys: List[int]
+    start_messages: List[MidiMessage]
+    end_messages: List[MidiMessage]
+    duration_min: Optional[int] = None
+    duration_max: Optional[int] = None
+    fallback_event: Optional[str] = None
 
