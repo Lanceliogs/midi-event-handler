42f63e222cf2d03966aee0431c3f1eadca4a8bbd
Julien LANCELOT <julien.lancelot@enovasense.com>
2025-07-29 09:33:48 +0200

    minor: very basic unit tests added

---
+++ tests/test_api.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+from midi_event_handler.core.config.loader import *
+
+test_mapping_path = Path(__file__).parent / "test_mapping.yaml"
+os.environ["MEH_MAPPING_YAML"] = str(test_mapping_path)
+load_mapping_yaml() # test load from env 
+
+from fastapi.testclient import TestClient
+from midi_event_handler.web.app import app
+client = TestClient(app)
+
+def test_mapping():
+    assert "InputDevice2" in get_configured_inputs()
+    assert get_event_types() == ["light", "music"]
+    events = get_event_list()
+    assert len(events) == 3
+    assert events[0].chord.notes == [127, 131]
+
+def test_get_status():
+    response = client.get("/status")
+    assert response.status_code == 200
+    assert isinstance(response.json(), dict)
+
