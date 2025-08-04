6876566e4c8a0e2b96184145ecae36eeba34a726
Julien LANCELOT <julien.lancelot@enovasense.com>
2025-08-04 21:57:58 +0200

    minor: whole tray moved into launcher so it's self-contained and the exe is smaller

--- src/midi_event_handler/launcher.py
+++ src/midi_event_handler/launcher.py
@@ -6,12 +6,41 @@ import yaml
 import webbrowser
 import builtins
 
-from midi_event_handler.tools.tray import setup_tray_icon
+import threading
+from pystray import Icon, MenuItem as Item, Menu
+from PIL import Image
 
+# --- Parameters ----------------------------------------------------------
 is_compiled = '__compiled__' in globals()
 
 RUNTIME_DIR = Path(".runtime")
 RESTART_FLAG = RUNTIME_DIR / "restart.flag"
+EXIT_FLAG = RUNTIME_DIR / "exit.flag"
+
+ICON_PATH = Path("meh-icon.ico")
+
+def setup_tray_icon(url: str):
+    image = Image.open(ICON_PATH)
+
+    icon = Icon(
+        "midi_event_handler",
+        image,
+        "MIDI Event Handler",
+        menu=Menu(
+            Item("Open Dashboard", lambda icon, item:  webbrowser.open(url)),
+            Item("Quit", on_exit)
+        )
+    )
+
+    # Run the icon in a separate thread so it doesn't block
+    threading.Thread(target=icon.run, daemon=True).start()
+
+def on_exit(icon, item):
+    icon.stop()
+    exit_flag = Path(".runtime") / "exit.flag"
+    exit_flag.touch()
+
+# --- App config -------------------------------------------------
 
 conf = {}
 with open("config.yaml", "r") as f:
@@ -49,12 +78,17 @@ def launch_app():
     print(f"Starting app with command:\n  > {' '.join(command)}")
     return subprocess.Popen(command)
 
-def watch_for_restart():
+
+def watch_for_flag():
     while True:
         if RESTART_FLAG.exists():
-            print("Restart flag detected.")
-            return True
+            print("Restart flag detected")
+            return "restart"
+        if EXIT_FLAG.exists():
+            print("Exit flag detected")
+            return "exit"
         time.sleep(0.5)
+
 
 def main():
     url = f"http://127.0.0.1:{app_conf.get('port', 8000)}/dashboard"
@@ -65,12 +99,22 @@ def main():
     RUNTIME_DIR.mkdir(exist_ok=True)
     while True:
         proc = launch_app()
-        if watch_for_restart():
-            proc.terminate()
-            print(f"App returned {proc.wait()}")
+        flag = watch_for_flag()
+
+        # It's either restart or exit
+        proc.terminate()
+        print(f"App returned {proc.wait()}")
+        if flag == "restart":
             RESTART_FLAG.unlink(missing_ok=True)
+        elif flag == "exit":
+            EXIT_FLAG.unlink(missing_ok=True)
+            break
         else:
             break
+    
+    # We kill the flags at the end if needed
+    RESTART_FLAG.unlink(missing_ok=True)
+    EXIT_FLAG.unlink(missing_ok=True)
 
 if __name__ == "__main__":
     main()
