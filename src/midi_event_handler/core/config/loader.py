75c8b03a58857fa5a5daddfbd1f80418f764905a
Julien LANCELOT <julien.lancelot@enovasense.com>
2025-11-29 13:18:51 +0100

    minor: bad event should not break the whole file

--- src/midi_event_handler/core/config/loader.py
+++ src/midi_event_handler/core/config/loader.py
@@ -1,7 +1,7 @@
 import yaml
 import os
 from pathlib import Path
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Dict, Any
 
 from midi_event_handler.core.events.models import MidiChord, MidiEvent, MidiMessage
 
@@ -68,27 +68,30 @@ def get_event_types() -> List[str]:
 
 
 def get_event_list() -> List[MidiEvent]:
-    events = []
+    events: List[MidiEvent] = []
 
     for e in _raw.get("events", []):
-        chord = MidiChord(
-            notes=e["trigger"]["notes"],
-            port=e["trigger"]["port"]
-        )
+        try:
+            chord = MidiChord(
+                notes=e["trigger"]["notes"],
+                port=e["trigger"]["port"]
+            )
 
-        start = [MidiMessage(**m) for m in e.get("start_messages", [])]
-        end = [MidiMessage(**m) for m in e.get("end_messages", [])]
+            start = [MidiMessage(**m) for m in e.get("start_messages", [])]
+            end = [MidiMessage(**m) for m in e.get("end_messages", [])]
 
-        event = MidiEvent(
-            name=e["name"],
-            type=e["type"],
-            chord=chord,
-            start_messages=start,
-            end_messages=end,
-            duration_min=e.get("duration_min"),
-            duration_max=e.get("duration_max"),
-            fallback_event=e.get("fallback_event"),
-        )
-        events.append(event)
+            event = MidiEvent(
+                name=e["name"],
+                type=e["type"],
+                chord=chord,
+                start_messages=start,
+                end_messages=end,
+                duration_min=e.get("duration_min"),
+                duration_max=e.get("duration_max"),
+                fallback_event=e.get("fallback_event"),
+            )
+            events.append(event)
+        except:
+            log.error("Event cannot be loaded: %s", e)
 
     return events
